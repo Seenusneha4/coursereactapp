50dba6148912ef263c83ef0824dcc125a6a43feb
seenusneha4 <seenaannmathew6@gmail.com>
2022-05-19 11:22:28 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addcourse from './Components/Addcourse';
+import Header from './Components/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div>
-      <Addcourse/>
+     <BrowserRouter>
+     <Routes>
+       <Route path="/" exact element={<Addcourse/>}/>
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
